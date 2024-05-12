a3ab6906a6ecffc33cce87a1db4643ac07f9509b
Iljadut <121857275+Iljadut@users.noreply.github.com>
2024-05-12 13:47:25 +0200

    DarkMode

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <DarkModeToggleComponent />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import DarkModeToggleComponent from './components/DarkMode.vue'; // Hier wurde der Importpfad aktualisiert
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    DarkModeToggleComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-app/src/components/DarkMode.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="toggleDarkMode">Darkmode {{ darkMode ? 'ausschalten' : 'einschalten' }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DarkModeToggleComponent',
+  data() {
+    return {
+      darkMode: false
+    };
+  },
+  methods: {
+    toggleDarkMode() {
+      // Darkmode ein- oder ausschalten
+      this.darkMode = !this.darkMode;
+
+    }
+  }
+};
+</script>
